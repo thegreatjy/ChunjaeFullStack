35c1d9283572f97a5712274fe379fd5e06e6917b
thegreatjy <thegreatjy@gmail.com>
2023-10-05 20:29:44 +0900

    chapter5 solutions for examples

---
+++ javaProblems/Chapter5/Balance.java
@@ -0,0 +1,13 @@
+package javaProblems.Chapter5;
+
+public class Balance {
+    public static void main(String[] args) {
+        int[] coinUnit = {500, 100, 50, 10};
+        int money = 2680;
+        System.out.println("money="+money);
+        for(int i=0;i<coinUnit.length;i++) {
+            System.out.println(coinUnit[i]+"원 : " + money/coinUnit[i]);
+            money = money%coinUnit[i];
+        }
+    }
+}
