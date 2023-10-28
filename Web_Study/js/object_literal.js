5fab26e0b26b462271baa8d7dacc70681c1efd83
thegreatjy <thegreatjy@gmail.com>
2023-10-29 00:02:27 +0900

    practices in the class for web programming

---
+++ Web_Study/js/object_literal.js
@@ -0,0 +1,14 @@
+let studentScores = {
+	korean: 80,
+	english: 100,
+	math: null,
+    science: undefined,
+    art: '알 수 없음'
+};
+
+console.log(studentScores.korean);
+
+let emptyString = '';
+console.log(typeof(emptyString));
+console.log(typeof('asb'));
+console.log('string' === emptyString);
