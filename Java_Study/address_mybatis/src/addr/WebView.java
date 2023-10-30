b7c0a145561199e45c26b4ef59b0955e25ca31b1
thegreatjy <thegreatjy@gmail.com>
2023-10-30 11:07:13 +0900

    update app view

--- Java_Study/address_mybatis/src/addr/WebView.java
+++ Java_Study/address_mybatis/src/addr/WebView.java
@@ -8,7 +8,7 @@ public class WebView {
 		
 		System.out.println("웹 브라우저에 출력");
 		for(int i = 0; i < addrList.size(); i++) {
-			System.out.println("========= 주소록 목록 =========");
+			System.out.println("========= 주소록 목록 ==========");
 			System.out.println("번호 : " + addrList.get(i).getId());
 			System.out.println("이름 : " + addrList.get(i).getName());
 			System.out.println("주소 : " + addrList.get(i).getAddress());
