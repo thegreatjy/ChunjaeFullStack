edb7a0a060162125377b669e68956b078b47fafd
thegreatjy <thegreatjy@gmail.com>
2023-10-31 16:40:19 +0900

    add dto, mapper for board

--- Java_Study/thisisjavaDB/src/board/BoardMapper.java
+++ Java_Study/thisisjavaDB/src/board/BoardMapper.java
@@ -1,10 +1,12 @@
 package board;
 
+import thisisjava.ObjectMapper;
+
 import java.util.ArrayList;
 
-public interface BoardMapper {
+public interface BoardMapper extends ObjectMapper {
     public ArrayList<Board> getBoards();
-    public void insertBoards (Board board);
-    public void updateBoards (Board board);
-    public void deleteBoards (int id);
+    public void insertBoard (Board board);
+    public void updateBoard (Board board);
+    public void deleteBoard (int id);
 }
