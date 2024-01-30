a60148ace4a220772ef92abf7be1f778a0780917
thegreatjy <thegreatjy@gmail.com>
2024-01-30 09:09:25 +0900

    Add: swagger 사용해보기

---
+++ SpringBoot_Study/swaggerTest/src/test/java/com/study/swaggertest/SwaggerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.swaggertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
